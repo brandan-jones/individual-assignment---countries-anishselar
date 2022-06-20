a1afa1de9d9a975fab71033264816ff701f5e9b0
Anish Selar <anish.selar@gmail.com>
2022-06-19 22:15:46 -0400

    updated classes

--- app/src/main/java/app/plantdiary/individualassignment304832/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/individualassignment304832/MainViewModel.kt
@@ -1,20 +1,21 @@
 package app.plantdiary.individualassignment304832
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import app.plantdiary.individualassignment304832.dto.Country
 import app.plantdiary.individualassignment304832.service.CountryService
-
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    var countries: MutableLiveData<ArrayList<Country>> = MutableLiveData<ArrayList<Country>>()
-    var countryService: CountryService = CountryService()
-
-    init {
-        fetchCountries()
+    var countries : MutableLiveData<List<Country>> = MutableLiveData<List<Country>>()
+    var countryService : CountryService = CountryService()
+    fun fetchCountries(){
+        viewModelScope.launch {
+            var innerCountries = countryService.fetchCountries()
+            countries.postValue(innerCountries)
+        }
     }
 
-    fun fetchCountries() {
-        countries = countryService.fetchCountries("Zambia")
-    }
 }
--- app/src/main/java/app/plantdiary/individualassignment304832/service/CountryService.kt
+++ app/src/main/java/app/plantdiary/individualassignment304832/service/CountryService.kt
@@ -1,37 +1,22 @@
 package app.plantdiary.individualassignment304832.service
 
-import androidx.lifecycle.MutableLiveData
+
 import app.plantdiary.individualassignment304832.RetrofitClientInstance
 import app.plantdiary.individualassignment304832.dto.Country
 import app.plantdiary.individualassignment3048q.dao.ICountryDAO
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import retrofit2.awaitResponse
 
 class CountryService {
-    fun fetchCountries(countryName: String): MutableLiveData<ArrayList<Country>> {
-        var countries = MutableLiveData<ArrayList<Country>>()
-
-        val service = RetrofitClientInstance.retrofitInstance?.create(ICountryDAO::class.java)
-
-        val call = service?.fetchCountries()
-
-        call?.enqueue(object : Callback<ArrayList<Country>> {
-
-            override fun onResponse(
-                call: Call<ArrayList<Country>>,
-                response: Response<ArrayList<Country>>
-            ) {
-                countries.value = response.body()
-            }
-
-            override fun onFailure(call: Call<ArrayList<Country>>, t: Throwable) {
-                val j = 1 + 1
-                val i = 1 + 1
-            }
-        })
-
-        return countries
+    suspend fun fetchCountries(): List<Country>? {
+        return withContext(Dispatchers.IO) {
+            val service = RetrofitClientInstance.retrofitInstance?.create(ICountryDAO::class.java)
+            val countries = async { service?.fetchCountries() }
+            var results = countries.await()?.awaitResponse()?.body()
+            return@withContext results
+        }
     }
 }
 
